7ef758beaad623e10c6c7ce0bb753e03636e7abf
rchan <shihping.chan@gmail.com>
2017-07-13 00:23:08 +0800

    add file root

---
+++ fileroot/FILEROOT.md
@@ -0,0 +1,3 @@
+# File Root
+
+A filesystem hierarchy that mimics the files created during the transcripts. (Less large binary files...)
---
+++ fileroot/opt/install/manage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+## manage.sh
+## This is a nuke-n-pave script.
+## It stops and deletes all docker containers and 
+## removes all configuration by kubeadm
+
+function usage {
+    echo "Usage: manage.sh cleanup|stop|wipe"
+    echo "stop: stops all docker containers"
+    echo "cleanup: removes ALL docker containers"
+    echo "wipe: removes all kubernetes configuration set up by kubeadm"
+}
+
+[ -n "$1" ] || { usage; exit 1; }
+
+if [ $1 = 'cleanup' ]; then
+   docker rm $(docker ps -a --format '{{.Names}}')
+   exit 0
+fi
+
+if [ $1 = 'stop' ]; then
+   docker stop $(docker ps --format '{{.Names}}')
+   exit 0
+fi
+
+if [ $1 = 'wipe' ]; then
+   umount /var/lib/kubelet/*/*/*/*/*
+   rm -rf /var/lib/etcd/* /etc/kubernetes/* /var/lib/kubelet/*
+   exit 0
+fi
+
